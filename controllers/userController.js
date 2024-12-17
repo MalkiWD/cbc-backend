c05779d4efb3a3f05326e1bb9afdfb1c085cb7ba
malki nuwanthika <nuwanthikawee19@gmail.com>
2024-12-17 16:30:20 +0530

    create validation function

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,6 +8,24 @@ export function createUser(req,res){
 
   const newUserData = req.body
 
+  if(newUserData.type == "admin"){
+    if(req.user==null){
+      res.json({
+        message: "Please login as administrator to create admin accounts"
+      })
+      return
+    }
+    if(req.user.type != "admin"){
+      res.json({
+        message: "Please login as administrator to create admin accounts"
+      })
+      return
+    }
+  }
+
+
+
+
   newUserData.password = bcrypt.hashSync(newUserData.password,10)
 
   const user = new User(newUserData)
@@ -64,5 +82,25 @@ export function createUser(req,res){
       }
     }
   ) 
+}
+
+export function isAdmin(req){
+  if(req.user==null){
+    return false
+  }
+  if(req.user.type != "admin"){
+    return false
+  }
+  return true
+}
+
+export function isCustomer(req){
+  if(req.user==null){
+    return false
+  }
+  if(req.user.type != "customer"){
+    return false
+  }
+  return true
 }
 
